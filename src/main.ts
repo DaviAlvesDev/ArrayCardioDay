86ecf18c6a35dc244bd17bc5b071ac61af0d7e4d
Davi Alves <davialvesvilar2010@gmail.com>
2026-01-07 10:39:27 -0300

    :sparkles: Feat: Exercício #1 resolvido utilizando .filter()

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+interface inventor {
+    nome:string
+    sobrenome:string
+    nascimento:number
+    morte:number
+}
+
+const inventores: (inventor)[] = [
+    {nome: "Albert", sobrenome: "Einstein", nascimento: 1879, morte: 1955},
+    {nome: "Isaac", sobrenome: "Newton", nascimento: 1643, morte: 1727},
+    {nome: "Galileo", sobrenome: "Galilei", nascimento: 1564, morte: 1642},
+    {nome: "Marie", sobrenome: "Curie", nascimento: 1867, morte: 1934},
+    {nome: "Johannes", sobrenome: "Kepler", nascimento: 1571, morte: 1630},
+    {nome: "Nicolaus", sobrenome: "Copernicus", nascimento: 1473, morte: 1543},
+    {nome: "Max", sobrenome: "Planck", nascimento: 1858, morte: 1947}
+]
+
+const pessoas: (string)[] = [
+    'Bernhard, Sandra', 'Bethea, Erin', 'Becker, Carl', 'Bentsen, Lloyd', 'Beckett, Samuel', 'Blake, William', 'Berger, Ric', 'Beddoes, Mick', 'Beethoven, Ludwig',
+    'Belloc, Hilaire', 'Begin, Menachem', 'Bellow, Saul', 'Benchley, Robert', 'Blair, Robert', 'Benenson, Peter', 'Benjamin, Walter', 'Berlin, Irving',
+    'Benn, Tony', 'Benson, Leana', 'Bent, Silas', 'Berle, Milton', 'Berry, Halle', 'Biko, Steve', 'Beck, Glenn', 'Bergman, Ingmar', 'Black, Elk', 'Berio, Luciano',
+    'Berne, Eric', 'Berra, Yogi', 'Berry, Wendell', 'Bevan, Aneurin', 'Ben-Gurion, David', 'Bevel, Ken', 'Biden, Joseph', 'Bennington, Chester', 'Bierce, Ambrose',
+    'Billings, Josh', 'Birrell, Augustine', 'Blair, Tony', 'Beecher, Henry', 'Biondo, Frank'
+]
+
+const array1: inventor[] = inventores.filter((inventor) => Math.trunc(inventor.nascimento/100) == 15)
+
+console.log("Inventores do século 16:")
+array1.forEach((element) => console.log(element.nome, element.sobrenome))
+console.log("\n")
